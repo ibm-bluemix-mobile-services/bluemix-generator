9bf8c1c82bf67716d4d31e1cf4bfc7cd9e7a2a04
jmeis <jmeis@users.noreply.github.com>
2016-10-09 19:02:33 -0500

    Cloudant now returning 202s

--- modules/cloudant.js
+++ modules/cloudant.js
@@ -41,6 +41,8 @@
 				.end(function (err, res) {
 					if (!err && res.statusCode === 201) {
 						resolve();
+					} else if (res.statusCode === 202) {
+						resolve();
 					} else if (res.statusCode === 503) {
 						reject("The Cloudant NoSQL DB service is currently unavailable. Our technicians are working on fixing this issue as soon as possible. Please try running bluegen again at a later time.");
 					} else {
