d6c5683bfb4447c3ea32a421a6f50f852d96bb38
jmeis <jmeis@us.ibm.com>
2017-01-17 21:03:34 -0600

    Update copyright

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 /*
- * Copyright 2016 IBM Corp.
+ * Copyright 2016, 2017 IBM Corp.
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
